c41a47469132fed26225562f58a755a5f5ea043c
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-02-05 17:35:00 +0100

    jour 01 job02

---
+++ jour01/job02.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/*Job 2
+Modifier votre classe “Operation” afin d’y ajouter la méthode “addition()”
+. Cette
+méthode additionne “nombre1” et “nombre2” et afficher le résultat.*/
+
+
+//
+
+
+class Operation {
+    public $nombre1;
+    public $nombre2;
+
+    public function __construct($nombre1 = 0, $nombre2 = 0) {
+        $this->nombre1 = $nombre1;
+        $this->nombre2 = $nombre2;
+    }
+
+    public function afficher() {
+        echo "Nombre 1 : " . $this->nombre1 . "<br>";
+        echo "Nombre 2 : " . $this->nombre2 . "<br>";
+    }
+}
+
+// Création d'un objet avec les valeurs par défaut (0,0)
+$operation1 = new Operation();
+$operation1->afficher();
+
+echo "<br>";
+
+// Création d'un objet avec des valeurs spécifiques
+$operation2 = new Operation(5, 10);
+$operation2->afficher();
+?>
